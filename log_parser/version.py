1035fc925684f99d87e34a69642b80a49cfca7e5
Adeel Ahmad <230291+adeelahmad@users.noreply.github.com>
2022-11-10 18:14:03 +1100

    Update version.py

--- log_parser/version.py
+++ log_parser/version.py
@@ -8,4 +8,4 @@
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2022, Adeel Ahmad <230291+adeelahmad@users.noreply.github.com>
 
-__version__ = '0.1.0'  # NOQA
+__version__ = '0.0.3'  # NOQA
